3fa1bcba94b07cbe65be9af749de83c86a91d679
dagadeta <treriwin@gmail.com>
2024-01-28 11:00:45 +0100

    added information to album site

--- musik/album/code.js
+++ musik/album/code.js
@@ -15,7 +15,17 @@ async function replace() {
     document.title = albumdata[album].name + " | Rummelbude";
     document.getElementById("name").innerHTML = albumdata[album].name;
     document.getElementById("albumcover").src = "../../images/albums/200x200/" + albumdata[album].albumcover;
+
     document.getElementById("streamingheader").innerHTML = albumdata[album].name + " streamen";
+    document.getElementById("streamingplatforms").innerHTML = albumdata[album].streamingplatforms;
+    document.getElementById("streaminglinks").innerHTML = albumdata[album].streaminglinks;
+
+    document.getElementById("publishdate").innerHTML = albumdata[album].publishdate;
+    document.getElementById("songcount").innerHTML = albumdata[album].songcount;
+    document.getElementById("length").innerHTML = albumdata[album].length;
+    document.getElementById("instrumental").innerHTML = albumdata[album].instrumental + "/" + albumdata[album].songcount;
+
+    document.getElementById("songs").innerHTML = albumdata[album].songs;
 }
 
 function getURLparams() {
