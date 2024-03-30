5003a66ec1086e8b9510ceada5cafb5cf88a7cb3
dagadeta <dagadeta@web.de>
2024-03-30 17:14:35 +0100

    Correct href direction

--- musik/album/code.js
+++ musik/album/code.js
@@ -45,21 +45,21 @@ document.write('\
                 </tr>\
                 <tr>\
                     <th><a href="mailto:sup.rummelbude_musik@gmx.de">E-Mail</a></th>\
-                    <th><a href="musik">Musik</a></th>\
+                    <th><a href="/website/musik">Musik</a></th>\
                     <th>Copyright © 2023-2024 Rummelbude</th>\
                 </tr>\
                 <tr>\
-                    <th><a href="support">Support</a></th>\
-                    <th><a href="diskografie">Diskografie</a></th>\
+                    <th><a href="/website/support">Support</a></th>\
+                    <th><a href="/website/diskografie">Diskografie</a></th>\
                     <th>(sofern nicht anders angegeben)</th>\
                 </tr>\
                 <tr>\
                     <th></th>\
-                    <th><a href="support">Support</a></th>\
+                    <th><a href="/website/support">Support</a></th>\
                 </tr>\
                 <tr>\
                     <th></th>\
-                    <th><a href="newsletter">Newsletter</a></th>\
+                    <th><a href="/website/newsletter">Newsletter</a></th>\
                 </tr>\
             </table>\
         </div>\
