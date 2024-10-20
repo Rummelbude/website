5dc0a06d583fba602424f885a59fe06e2d30c067
dagadeta <dagadeta@web.de>
2024-10-20 17:07:41 +0200

    Revise the style for the album page

--- musik/album/code.js
+++ musik/album/code.js
@@ -2,28 +2,58 @@ function redirect(url) {
     window.location.href = url;
 }
 
-async function replace() {
+async function insertData() {
     let albumdata;
     let album;
+
+    // Get the album data
     await fetch('./albumdata.json')
         .then((response) => response.json())
         .then((json) => albumdata = json);
     album = getURLparams().get("album");
 
+    // Insert name and picture
     document.title = albumdata[album].name + " | Rummelbude";
     document.getElementById("name").innerHTML = albumdata[album].name;
     document.getElementById("albumcover").src = "../../images/albums/" + albumdata[album].id + ".jpg";
 
+    // Insert streaming links
     document.getElementById("streamingheader").innerHTML = albumdata[album].name + " streamen";
-    document.getElementById("streamingplatforms").innerHTML = albumdata[album].streamingplatforms;
-    document.getElementById("streaminglinks").innerHTML = albumdata[album].streaminglinks;
+    albumdata[album].streamingplatforms.forEach((platform, index) => {
+        const button = document.createElement('button');
+        button.innerHTML = platform;
+        button.onclick = function() {
+            redirect(albumdata[album].streaminglinks[index]);
+        };
 
+        document.getElementById('streamingcontainer').appendChild(button);
+        document.getElementById('streamingcontainer').appendChild(document.createElement("br"));
+    });
+
+    // Insert general information
     document.getElementById("publishdate").innerHTML = albumdata[album].publishdate;
     document.getElementById("songcount").innerHTML = albumdata[album].songcount;
     document.getElementById("length").innerHTML = albumdata[album].length;
     document.getElementById("instrumental").innerHTML = albumdata[album].instrumental + "/" + albumdata[album].songcount;
 
-    document.getElementById("songs").innerHTML = albumdata[album].songs;
+    // Insert song list
+    const tableBody = document.querySelector('#songtable tbody');
+    albumdata[album].songs.forEach((song, index) => {
+        const row = document.createElement('tr');
+
+        const numberCell = document.createElement('td');
+        numberCell.className = "numbercell";
+        numberCell.textContent = index + 1;
+
+        const nameCell = document.createElement('td');
+        nameCell.className = "namecell";
+        nameCell.textContent = song;
+
+        row.appendChild(numberCell);
+        row.appendChild(nameCell);
+
+        tableBody.appendChild(row);
+    });
 }
 
 function getURLparams() {
@@ -32,7 +62,7 @@ function getURLparams() {
     return(urlParams);
 }
 
-replace()
+insertData()
 
 document.write('\
 \
