221410741cbefb1b79082d60fef1240538d76391
dagadeta <dagadeta@web.de>
2024-12-02 21:54:23 +0100

    Improve navigation menu
    This commit includes:
    - The addition of the sections "Links" and "Rechtliches" in the navigation menu
    - The centralization of the navigation menu html
    - A fix to the GitHub link

--- resources/navigation/code.js
+++ resources/navigation/code.js
@@ -1,3 +1,32 @@
 function navigationControl(display) {
     document.getElementById("menu").style.display = display;
 }
+
+async function insertData() {
+    let links;
+
+    await fetch('/website/links/links.json')
+        .then((response) => response.json())
+        .then((json) => links = json);
+
+    const insertionDiv = document.getElementById('socialMediaPlatforms');
+    for (const key in links) {
+        if (links.hasOwnProperty(key)) {
+            const { name, link } = links[key];
+
+            const button = document.createElement("button");
+            button.textContent = name;
+
+            const linkElement = document.createElement("a");
+            linkElement.href = link;
+
+            linkElement.appendChild(button);
+            insertionDiv.appendChild(linkElement);
+        }
+    }
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    insertData();
+});
