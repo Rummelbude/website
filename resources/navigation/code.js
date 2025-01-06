41db97675494a9b76d06bd8b999dd8c1c185847a
dagadeta <dagadeta@web.de>
2025-01-06 18:55:14 +0100

    Redesign page header and navigation menu
    This commit includes:
    - The complete redesign of page headers and the navigation menu

--- resources/navigation/code.js
+++ resources/navigation/code.js
@@ -1,16 +1,41 @@
-function navigationControl(display) {
-    document.getElementById("menu").style.display = display;
+function navigationControl() {
+    const menu = document.getElementById("menu");
+
+    if (menu.classList.contains('hidden')) {
+        menu.classList.remove('hidden');
+        menu.style.display = 'block';
+        setTimeout(function () {
+            menu.classList.remove('visuallyHidden');
+        }, 20);
+
+        document.addEventListener('keydown', handleEsc);
+    } else {
+        hideNavigation();
+    }
 
     function handleEsc(event) {
         if (event.key === 'Escape') {
-            document.getElementById("menu").style.display = "none";
+            hideNavigation();
             document.removeEventListener('keydown', handleEsc);
         }
     }
+}
 
-    if (display === "block") {
-        document.addEventListener('keydown', handleEsc);
-    }
+function hideNavigation() {
+    const menu = document.getElementById("menu");
+    const menuButton = document.getElementById("headerMenuButton");
+
+    menu.classList.add('visuallyHidden');
+    menu.addEventListener('transitionend', function(e) {
+        menu.classList.add('hidden');
+        menu.style.display = 'none';
+    }, {
+        capture: false,
+        once: true,
+        passive: false
+    });
+
+    menuButton.checked = false;
 }
 
 async function insertLinks() {
@@ -22,7 +47,7 @@ async function insertLinks() {
 
     const insertionDiv = document.getElementById('navigationSocialMedia');
     for (const key in links) {
-        if (links.hasOwnProperty(key)) {
+        if (links.hasOwnProperty(key) && key !== "website") {
             const { name, link } = links[key];
 
             const button = document.createElement("button");
