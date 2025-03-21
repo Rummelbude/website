f8a0d8f803f01e0a8184248a0b27b5b0f850ccaf
dagadeta <dagadeta@web.de>
2025-03-21 12:34:05 +0100

    Change default header text on album page

--- musik/album/code.js
+++ musik/album/code.js
@@ -35,9 +35,9 @@ async function insertData() {
 
     // Insert name and picture
     document.title = albumData.name + " | Rummelbude";
-    document.getElementById("name").innerHTML = albumData.name;
+    document.getElementById("name").innerHTML = ` > ${albumData.name}`;
     await adjustMenuPosition();
-    document.getElementById("albumCover").src = "../../images/albums/" + albumData.id + ".jpg";
+    document.getElementById("albumCover").src = `../../images/albums/${albumData.id}.jpg`;
     document.getElementById("albumCover").classList.remove("albumPageContentHidden");
     document.getElementById("biggerAlbumHint").classList.remove("albumPageContentHidden");
 
