35dbb0648d738670a24bd70b43b21d81282acb51
dagadeta <dagadeta@web.de>
2025-05-26 14:53:33 +0200

    Refactor `insertData()` function

--- musik/album/code.js
+++ musik/album/code.js
@@ -32,61 +32,78 @@ async function getAlbumData(album) {
 async function insertData() {
     const albumData = await getAlbumData(album);
 
-    // Insert name and picture
-    document.title = albumData.name + " | Rummelbude";
-    document.getElementById("name").innerHTML = ` > ${albumData.name}`;
-    await adjustMenuPosition();
-    document.getElementById("albumCover").src = `../../images/albums/${albumData.id}.jpg`;
-    document.getElementById("albumCover").classList.remove("albumPageContentHidden");
-    document.getElementById("biggerAlbumHint").classList.remove("albumPageContentHidden");
+    await insertNameAndPicture();
 
     // Streaming section
-
-    // Insert streaming links
-    document.getElementById("streamingHeader").innerHTML = albumData.name + " streamen";
-    const streamingLogos = await getAvailableStreamingLogos();
-
-    if (Object.keys(albumData.streaming).length === 0) {
-        const infoMessage = document.createElement("p");
-        infoMessage.innerHTML = `${albumData.name} ist nicht mehr oder noch nicht zum Streamen verfügbar.`;
-        infoMessage.id = "streamingInfoMessage";
-        infoMessage.classList.add("textBottom");
-        document.getElementById('streamingContainer').appendChild(infoMessage);
-    } else {
-        Object.entries(albumData.streaming).forEach(([platform, url]) => {
-            const button = document.createElement('button');
-
-            if (streamingLogos[platform]) {
-                const logo = document.createElement('img');
-                logo.src = "../../images/links/" + streamingLogos[platform];
-                logo.classList.add("streamingButtonWithLogo");
-                button.style.fontSize = "smaller";
-
-                button.appendChild(logo);
-            }
-
-            const buttonText = document.createElement('span');
-            buttonText.innerText = platform;
-            button.appendChild(buttonText);
-
-            const a = document.createElement('a');
-            a.href = url.toString();
-            a.target = "_blank";
-            a.appendChild(button);
-
-            document.getElementById('streamingContainer').appendChild(a);
-        });
-    }
-
-
-    // Insert the clickable video thumbnail if a video exists
+    await insertStreamingLinks();
     if (albumData.video !== "") {
+        insertVideoThumbnail();
+    }
+    document.getElementById("streamingSection").classList.remove("albumPageContentHidden");
+
+    // General information
+    insertGeneralInformation();
+
+    // Songs
+    insertSongList();
+
+    // Show footer
+    document.getElementById("footer").style.display = "block";
+
+
+
+    // Functions
+    async function insertNameAndPicture() {
+        document.title = albumData.name + " | Rummelbude";
+        document.getElementById("name").innerHTML = ` > ${albumData.name}`;
+        await adjustMenuPosition();
+        document.getElementById("albumCover").src = `../../images/albums/${albumData.id}.jpg`;
+        document.getElementById("albumCover").classList.remove("albumPageContentHidden");
+        document.getElementById("biggerAlbumHint").classList.remove("albumPageContentHidden");
+    }
+    async function insertStreamingLinks() {
+        document.getElementById("streamingHeader").innerHTML = albumData.name + " streamen";
+        const streamingLogos = await getAvailableStreamingLogos();
+
+        if (Object.keys(albumData.streaming).length === 0) {
+            const infoMessage = document.createElement("p");
+            infoMessage.innerHTML = `${albumData.name} ist nicht mehr oder noch nicht zum Streamen verfügbar.`;
+            infoMessage.id = "streamingInfoMessage";
+            infoMessage.classList.add("textBottom");
+            document.getElementById('streamingContainer').appendChild(infoMessage);
+        } else {
+            Object.entries(albumData.streaming).forEach(([platform, url]) => {
+                const button = document.createElement('button');
+
+                if (streamingLogos[platform]) {
+                    const logo = document.createElement('img');
+                    logo.src = "../../images/links/" + streamingLogos[platform];
+                    logo.classList.add("streamingButtonWithLogo");
+                    button.style.fontSize = "smaller";
+
+                    button.appendChild(logo);
+                }
+
+                const buttonText = document.createElement('span');
+                buttonText.innerText = platform;
+                button.appendChild(buttonText);
+
+                const a = document.createElement('a');
+                a.href = url.toString();
+                a.target = "_blank";
+                a.appendChild(button);
+
+                document.getElementById('streamingContainer').appendChild(a);
+            });
+        }
+    }
+    function insertVideoThumbnail() {
         const videoThumbnailContainer = document.getElementById("videoThumbnailContainer");
         const videoThumbnail = document.createElement("img");
         videoThumbnail.src = "../../images/albums/video-thumbnails/" + albumData.id + "_videoThumbnail.jpg";
         videoThumbnail.id = "videoThumbnail";
         videoThumbnail.alt = "Video Thumbnail";
-        videoThumbnail.onclick = function() {
+        videoThumbnail.onclick = function () {
             window.location.href = albumData.video;
         };
 
@@ -95,39 +112,33 @@ async function insertData() {
         document.getElementById("videoHeader").style.display = "block";
         videoThumbnailContainer.style.display = "block";
     }
+    function insertGeneralInformation() {
+        document.getElementById("publishDate").innerHTML = albumData.publishDate;
+        document.getElementById("songCount").innerHTML = albumData.songCount;
+        document.getElementById("length").innerHTML = albumData.length;
+        document.getElementById("instrumental").innerHTML = albumData.instrumental + " von " + albumData.songCount;
+        document.getElementById("generalInfos").classList.remove("albumPageContentHidden");
+    }
+    function insertSongList() {
+        const tableBody = document.querySelector('#songTable tbody');
+        albumData.songs.forEach((song, index) => {
+            const row = document.createElement('tr');
 
-    document.getElementById("streamingSection").classList.remove("albumPageContentHidden");
+            const numberCell = document.createElement('td');
+            numberCell.className = "numberCell";
+            numberCell.textContent = (index + 1).toString();
 
+            const nameCell = document.createElement('td');
+            nameCell.className = "nameCell";
+            nameCell.textContent = song;
 
-    // Insert general information
-    document.getElementById("publishDate").innerHTML = albumData.publishDate;
-    document.getElementById("songCount").innerHTML = albumData.songCount;
-    document.getElementById("length").innerHTML = albumData.length;
-    document.getElementById("instrumental").innerHTML = albumData.instrumental + " von " + albumData.songCount;
-    document.getElementById("generalInfos").classList.remove("albumPageContentHidden");
+            row.appendChild(numberCell);
+            row.appendChild(nameCell);
 
-    // Insert song list
-    const tableBody = document.querySelector('#songTable tbody');
-    albumData.songs.forEach((song, index) => {
-        const row = document.createElement('tr');
-
-        const numberCell = document.createElement('td');
-        numberCell.className = "numberCell";
-        numberCell.textContent = (index + 1).toString();
-
-        const nameCell = document.createElement('td');
-        nameCell.className = "nameCell";
-        nameCell.textContent = song;
-
-        row.appendChild(numberCell);
-        row.appendChild(nameCell);
-
-        tableBody.appendChild(row);
-    });
-    document.getElementById("songs").classList.remove("albumPageContentHidden");
-
-    // Insert footer
-    document.getElementById("footer").style.display = "block";
+            tableBody.appendChild(row);
+        });
+        document.getElementById("songs").classList.remove("albumPageContentHidden");
+    }
 }
 
 function getQueryString() {
